a2626da713ba7f920fc7c37dbab4690bd54e5308
wangyuanmou <wangyuanmou@zhuozhuo.io>
2025-11-17 10:53:31 +0800

    update

--- examples/2d/mesh2d.rs
+++ examples/2d/mesh2d.rs
@@ -1,6 +1,6 @@
 //! Shows how to render a polygonal [`Mesh`], generated from a [`Rectangle`] primitive, in a 2D scene.
 
-use bevy::{color::palettes::basic::PURPLE, mesh::VertexAttributeValues, prelude::*};
+use bevy::{color::palettes::basic::PURPLE, prelude::*};
 
 fn main() {
     App::new()
@@ -10,6 +10,9 @@ fn main() {
         .run();
 }
 
+#[derive(Component)]
+struct PolygonSideCount(u32);
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -18,7 +21,8 @@ fn setup(
     commands.spawn(Camera2d);
 
     commands.spawn((
-        Mesh2d(meshes.add(RegularPolygon::new(1.0, 8))),
+        PolygonSideCount(3u32),
+        Mesh2d(meshes.add(RegularPolygon::new(1.0, 3u32))),
         MeshMaterial2d(materials.add(Color::from(PURPLE))),
         Transform::default().with_scale(Vec3::splat(128.)),
     ));
@@ -27,29 +31,16 @@ fn setup(
 fn input_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    query: Single<&Mesh2d>,
+    query: Single<(&mut Mesh2d, &mut PolygonSideCount)>,
 ) {
-    let Some(mesh) = meshes.get_mut(*query) else {
-        return;
-    };
-    let Some(VertexAttributeValues::Float32x3(positions)) =
-        mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION)
-    else {
-        return;
-    };
+    let (mut mesh, mut sides) = query.into_inner();
     if keyboard_input.just_pressed(KeyCode::ArrowDown) {
+        sides.0 -= if sides.0 > 3 { 1 } else { 0 };
+        mesh.0 = meshes.add(RegularPolygon::new(1.0, sides.0));
         println!("down");
-        for position in positions {
-            position[0] /= 2.0;
-            position[1] /= 2.0;
-            position[2] /= 2.0;
-        }
     } else if keyboard_input.just_pressed(KeyCode::ArrowUp) {
+        sides.0 += 1;
+        mesh.0 = meshes.add(RegularPolygon::new(1.0, sides.0));
         println!("up");
-        for position in positions {
-            position[0] *= 2.0;
-            position[1] *= 2.0;
-            position[2] *= 2.0;
-        }
     }
 }
