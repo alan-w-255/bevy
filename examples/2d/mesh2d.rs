a030d6ef8c47be2888686a0c8c48cc7987838d5a
wangyuanmou <wangyuanmou@zhuozhuo.io>
2025-11-18 15:33:45 +0800

    update

--- examples/2d/mesh2d.rs
+++ examples/2d/mesh2d.rs
@@ -1,6 +1,6 @@
 //! Shows how to render a polygonal [`Mesh`], generated from a [`Rectangle`] primitive, in a 2D scene.
 
-use bevy::{color::palettes::basic::PURPLE, prelude::*};
+use bevy::{color::palettes::basic::PURPLE, platform::collections::HashMap, prelude::*};
 
 fn main() {
     App::new()
@@ -9,6 +9,10 @@ fn main() {
         .add_systems(Update, input_system)
         .run();
 }
+
+/// Store the image handle that we will draw to, here.
+#[derive(Resource)]
+struct PolyMeshMap(HashMap<u32, Handle<Mesh>>);
 
 #[derive(Component)]
 struct PolygonSideCount(u32);
@@ -19,6 +23,7 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn(Camera2d);
+    commands.insert_resource(PolyMeshMap(HashMap::new()));
 
     commands.spawn((
         PolygonSideCount(3u32),
@@ -30,18 +35,25 @@ fn setup(
 
 fn input_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut poly_map: ResMut<PolyMeshMap>,
     mut meshes: ResMut<Assets<Mesh>>,
     query: Single<(&mut Mesh2d, &mut PolygonSideCount)>,
 ) {
     let (mut mesh, mut sides) = query.into_inner();
     if keyboard_input.just_pressed(KeyCode::ArrowDown) {
         sides.0 -= if sides.0 > 3 { 1 } else { 0 };
-        meshes.remove(mesh.0.id()); // resource 应该需要主动回收, 或者使用一个 map 记录并复用。
-        mesh.0 = meshes.add(RegularPolygon::new(1.0, sides.0));
-        println!("down");
     } else if keyboard_input.just_pressed(KeyCode::ArrowUp) {
         sides.0 += 1;
         mesh.0 = meshes.add(RegularPolygon::new(1.0, sides.0));
-        println!("up");
+    } else {
+        return;
+    }
+    if let Some(mesh_handle) = poly_map.0.get(&sides.0) {
+        mesh.0 = mesh_handle.clone();
+        println!("found cache");
+    } else {
+        let mesh_handle = meshes.add(RegularPolygon::new(1.0, sides.0));
+        poly_map.0.insert(sides.0, mesh_handle.clone());
+        mesh.0 = mesh_handle;
     }
 }
